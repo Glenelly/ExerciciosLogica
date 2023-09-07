69fb62c7faae24a2b5532728f16c9ed43846378f
Glenelly <glenellykarlatbm@gmail.com>
2023-09-07 14:55:35 -0300

    jeito diferente de calcular area e perimentro de um quadrado

---
+++ src/AreaPerimetroQuadradoComplexo.java
@@ -0,0 +1,22 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class AreaPerimetroQuadradoComplexo {
+
+    /*Dado o tamanho do lado de um quadrado, calcular a área e o perímetro do mesmo.*/
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o lado do quadrado: ");
+        BigDecimal lado = new BigDecimal(scan.nextInt());
+
+        BigDecimal perimetro = lado.add(lado).multiply(BigDecimal.valueOf(2));
+        BigDecimal area = lado.multiply(lado);
+
+        System.out.println("O perímetro do quadrado é: " + perimetro);
+        System.out.println("A área do quadrado é: " + area);
+
+    }
+}
